57af8c196f79cb076869a6d8e76a70971840e42e
Atika Rachmawati <rachmawati.atika@gmail.com>
2024-10-11 18:13:49 +0200

    add restaurant, bar, cafe

--- src/main/java/com/favourite/eatery/config/FavouriteEateryDatabase.java
+++ src/main/java/com/favourite/eatery/config/FavouriteEateryDatabase.java
@@ -1,8 +1,6 @@
 package com.favourite.eatery.config;
 
-import com.favourite.eatery.model.AppUser;
-import com.favourite.eatery.model.BusinessDayTime;
-import com.favourite.eatery.model.Eatery;
+import com.favourite.eatery.model.*;
 import com.favourite.eatery.repository.AppUserRepository;
 import com.favourite.eatery.repository.EateryRepository;
 import org.slf4j.Logger;
@@ -24,11 +22,14 @@ public class FavouriteEateryDatabase {
     CommandLineRunner initDatabase(EateryRepository eateryRepository, AppUserRepository userRepository) {
         return args -> {
             BusinessDayTime restaurant1Opening = new BusinessDayTime(DayOfWeek.MONDAY, LocalTime.of(18, 0), LocalTime.of(23, 0));
-            logger.info("Preloading {}", eateryRepository.save(new Eatery("restaurant-1", "address-restaurant-1", Set.of(restaurant1Opening), 100, "service@restaurant-1.com", "030-123-456")));
+            logger.info("Preloading {}", eateryRepository.save(new Restaurant("restaurant-1", "address-restaurant-1", Set.of(restaurant1Opening), 100, "service@restaurant-1.com", "030-123-456")));
 
             BusinessDayTime bar1Opening1 = new BusinessDayTime(DayOfWeek.SATURDAY, LocalTime.of(18, 0), LocalTime.MAX);
             BusinessDayTime bar1Opening2 = new BusinessDayTime(DayOfWeek.SUNDAY, LocalTime.MIDNIGHT, LocalTime.of(4, 0));
-            logger.info("Preloading {}", eateryRepository.save(new Eatery("bar-1", "address-bar-1", Set.of(bar1Opening1, bar1Opening2), 50, "hello@bar-1.de", "069-123-456")));
+            logger.info("Preloading {}", eateryRepository.save(new Bar("bar-1", "address-bar-1", Set.of(bar1Opening1, bar1Opening2), 50, "hello@bar-1.de", "069-123-456")));
+
+            BusinessDayTime cafe1Opening = new BusinessDayTime(DayOfWeek.FRIDAY, LocalTime.of(7, 0), LocalTime.of(15, 0));
+            logger.info("Preloading {}", eateryRepository.save(new Cafe("cafe-1", "address-cafe-1", Set.of(cafe1Opening), 20, "kontakt@cafe-1.com", "040-123-456")));
 
             logger.info("Preloading {}", userRepository.save(new AppUser("Max", "Mustermann", "mustermann@example.com", "0152-1234-5678")));
         };
