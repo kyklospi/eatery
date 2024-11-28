d1993a7ba2ad20bcac6b72738796e80c1acaabc2
Atika Rachmawati <rachmawati.atika@gmail.com>
2024-11-28 22:14:24 +0100

    fix test properties in mvn

--- src/test/java/com/eatery/api/controller/EateryControllerIntegrationTest.java
+++ src/test/java/com/eatery/api/controller/EateryControllerIntegrationTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.DayOfWeek;
 import java.time.LocalTime;
@@ -17,6 +18,7 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EateryControllerIntegrationTest {
     @Autowired
     EateryController eateryController;
