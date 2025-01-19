4987c3ee4f1880f66f4f93353edec473ba5bb516
kyklospi <rachmawati.atika@gmail.com>
2025-01-19 19:45:00 +0100

    small time json format fix

--- src/main/java/com/eatery/api/dto/UpdateReservationRequest.java
+++ src/main/java/com/eatery/api/dto/UpdateReservationRequest.java
@@ -21,7 +21,7 @@ import java.time.LocalDateTime;
 public class UpdateReservationRequest {
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
     private LocalDateTime dateTime;
     private int guestNumber;
 }
